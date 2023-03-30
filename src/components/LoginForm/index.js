8fab58e4375abdea707c6bf0e9aa177f6083b961
akshitha1305 <akshithamerugu321@gmail.com>
2023-03-30 13:34:23 +0000

    display console

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -44,6 +44,8 @@ class LoginForm extends Component {
     }
     const response = await fetch(url, options)
     const data = await response.json()
+    console.log(response)
+    console.log(data)
 
     if (response.ok === true) {
       this.onSubmitSuccess(data.jwt_token)
